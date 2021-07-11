a6b2266dd94c873238106c351086295da086347f
Даниил <korotkovdan@gmail.com>
2021-07-11 16:56:26 +0300

    The interface has been implemented. The calculation algorithm is not implemented.

--- Calulator/UI/MainWindow.xaml.cs
+++ Calulator/UI/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Calulator
 {
@@ -23,6 +10,121 @@ namespace Calulator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Keyboard_KeyClick(object sender, RoutedEventArgs e)
+        {
+            switch (sender.ToString())
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                    OutPutTextbox.AppendText(sender.ToString());
+                    break;
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    if (OutPutTextbox.Text.Length != 0 &&
+                        OutPutTextbox.Text[^1] != '+' &&
+                        OutPutTextbox.Text[^1] != '-' &&
+                        OutPutTextbox.Text[^1] != '*' &&
+                        OutPutTextbox.Text[^1] != '/')
+                    {
+                        OutPutTextbox.AppendText(sender.ToString());
+                    }
+                    break;
+                case ",":
+                    if (OutPutTextbox.Text.Length != 0 && CanInputSemi())
+                    {
+                        OutPutTextbox.AppendText(",");
+                    }
+                    break;
+                case "DEL":
+                    if (OutPutTextbox.Text.Length == 0) { return; }
+                    OutPutTextbox.Text = OutPutTextbox.Text.Remove(OutPutTextbox.Text.Length - 1);
+                    break;
+                case "C":
+                    OutPutTextbox.Clear();
+                    break;
+                case "+/-":
+                    if (OutPutTextbox.Text.Length == 0) { return; }
+                    if (OutPutTextbox.Text[0] != '-')
+                    {
+                        OutPutTextbox.Text = "-" + OutPutTextbox.Text;
+                    }
+                    else
+                    {
+                        OutPutTextbox.Text = OutPutTextbox.Text[1..];
+                    }
+                    break;
+                case "%":
+                    if (OutPutTextbox.Text.Length == 0) { return; }
+                    CalcPercent();
+                    break;
+                case "=":
+                    if(OutPutTextbox.Text.Length == 0) { return; }
+                    CalcResult();                    
+                    break;
+
+                default:
+                    MessageBox.Show(sender.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    break;
+            }
+        }
+
+        private void CalcPercent()
+        {
+
+        }
+
+        private void CalcResult()
+        {
+
+        }
+
+        private bool CanInputSemi()
+        {
+            if (OutPutTextbox.Text[^1] == ',')
+            {
+                return false;
+            }
+
+            if (!OutPutTextbox.Text.Contains('+') &&
+                !OutPutTextbox.Text.Contains('-') &&
+                !OutPutTextbox.Text.Contains('*') &&
+                !OutPutTextbox.Text.Contains('/'))
+            {
+                if (!OutPutTextbox.Text.Contains(','))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            string buf = OutPutTextbox.Text;
+            while (buf[^1] != '+' &&
+                   buf[^1] != '-' &&
+                   buf[^1] != '*' &&
+                   buf[^1] != '/')
+            {
+                if (buf[^1] == ',')
+                {
+                    return false;
+                }
+
+                buf = buf[0..^1];
+            }
+            return true;
         }
     }
 }
